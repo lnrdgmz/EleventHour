4cc15253e190d905e1610a5c200311672712e100
Leo Gomez <leogomez@gmail.com>
2017-05-19 14:31:58 -0400

    Move code into appropriate files

--- config/config.example.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  googleClientId: 'FILL_ME_IN',
-  googleSecret: 'FILL_ME_IN',
-};
-
-module.exports = config;
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,1 +1,6 @@
 // require all middleware here
+const authRoutes = require('../routes/authRoutes');
+
+module.exports = (app, express) => {
+  app.use(authRoutes);
+}
