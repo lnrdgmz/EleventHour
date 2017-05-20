918ceab6f2c5d14ccbebdf4d70d18f10fea8b2ac
Leo Gomez <leogomez@gmail.com>
2017-05-20 14:41:04 -0400

    Look up and/or add users to db after they authenticate with OAuth provider

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,22 +1,47 @@
 const authRouter = require('express').Router();
 const passport = require('passport');
 
+const User = require('../models/user');
 
-authRouter.get('/auth/google', passport.authenticate('google', {scope: ['profile']}));
+
+authRouter.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 authRouter.get('/auth/facebook', passport.authenticate('facebook'))
 
 
-authRouter.get('/auth/google/callback', 
-  passport.authenticate('google', {failureRedirect: '/fail'}),
+authRouter.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/fail' }),
   (req, res) => {
     /**
      * Set some cookie data and send the appropriate response code
      */
-    res.send(`Log in succeeded! Welcome, ${JSON.stringify(req.user)}`);
+    const user = {
+      oauth_provider: req.user.provider,
+      provider_id: req.user.id,
+    };
+
+    const userObj = new User(user);
+
+    userObj.fetch()
+      .then(model => {
+        console.log(`Searched for ${JSON.stringify(userObj)}`)
+        console.log(`and got ${JSON.stringify(model)}`)
+        if (model) {
+          res.send(`Found ${JSON.stringify(model)} in the database`);
+        } else {
+          // add necessary attributes to the user
+          userObj.set({display_name: req.user.displayName}).save()
+            .then(model => {
+              res.send(`Added ${JSON.stringify(model)} to the database`);
+            })
+
+        }
+      })
+
+    // res.send(`Log in succeeded! Welcome, ${JSON.stringify(req.user)}`);
   });
 
-authRouter.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', {failureRedirect: '/fail'}), 
+authRouter.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/fail' }),
   (req, res) => {
     /*  
      * Set some cookie data and send the appropriate response code
@@ -33,6 +58,6 @@ authRouter.get('/auth/loggedIn', (req, res) => {
   }
 });
 
-authRouter.get('/auth/logout', (req, res) => {/* Log user out */});
+authRouter.get('/auth/logout', (req, res) => {/* Log user out */ });
 
 module.exports = authRouter;
