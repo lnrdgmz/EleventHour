958bfbd20369393e8fac425f46684affce45ee22
Leo Gomez <leogomez@gmail.com>
2017-05-20 14:59:30 -0400

    Refactor auth callback handler to handle both Facebook and Google

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,26 @@ const passport = require('passport');
 
 const User = require('../models/user');
 
+const callbackHandler = (req, res) => {
+  /**
+   * Set some cookie data and send the appropriate response code
+   */
+  const userObj = new User({
+    oauth_provider: req.user.provider,
+    provider_id: req.user.id,
+  });
+  userObj.fetch()
+    .then(model => {
+      if (model) {
+        res.send(`Found ${JSON.stringify(model)} in the database`);
+      } else {
+        userObj.set({display_name: req.user.displayName}).save()
+          .then(model => {
+            res.send(`Added ${JSON.stringify(model)} to the database`);
+          })
+      }
+    })
+}
 
 authRouter.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 authRouter.get('/auth/facebook', passport.authenticate('facebook'))
@@ -10,45 +30,11 @@ authRouter.get('/auth/facebook', passport.authenticate('facebook'))
 
 authRouter.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/fail' }),
-  (req, res) => {
-    /**
-     * Set some cookie data and send the appropriate response code
-     */
-    const user = {
-      oauth_provider: req.user.provider,
-      provider_id: req.user.id,
-    };
-
-    const userObj = new User(user);
-
-    userObj.fetch()
-      .then(model => {
-        console.log(`Searched for ${JSON.stringify(userObj)}`)
-        console.log(`and got ${JSON.stringify(model)}`)
-        if (model) {
-          res.send(`Found ${JSON.stringify(model)} in the database`);
-        } else {
-          // add necessary attributes to the user
-          userObj.set({display_name: req.user.displayName}).save()
-            .then(model => {
-              res.send(`Added ${JSON.stringify(model)} to the database`);
-            })
-
-        }
-      })
-
-    // res.send(`Log in succeeded! Welcome, ${JSON.stringify(req.user)}`);
-  });
+  callbackHandler);
 
 authRouter.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/fail' }),
-  (req, res) => {
-    /*  
-     * Set some cookie data and send the appropriate response code
-     */
-    res.send(`Log in succeeded! Welcome, ${JSON.stringify(req.user)}`);
-  });
-
+  callbackHandler);
 
 authRouter.get('/auth/loggedIn', (req, res) => {
   if (req.isAuthenticated()) {
