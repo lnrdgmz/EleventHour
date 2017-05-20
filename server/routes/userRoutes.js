ba62d75b6d07df115d4c9f8767e4c1728dfca9b6
Leo Gomez <leogomez@gmail.com>
2017-05-20 10:23:36 -0400

    Add comments

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,19 +7,32 @@ const userUtils = require('../utils/userUtils.js')
 // Create route handles for users
 
 userRouter.route('/')
+    /**
+     * When would we need to return a list of all users?
+     */
     .get((req, res) => {
-        //return users
+        res.send('Returns a list of users.')
     })
+    /**
+     * Take a JSON object of user data
+     */
     .post((req, res) => {
-        //create user
+        res.send('Create a user')
     })
 
 userRouter.route('/:userId')
+    /**
+     * Respond with different information if the request is made by the user, another user,
+     * or someone who is not logged in?
+     */
+    .get((req, res) => {
+        res.send(`Respond with data of user ${userId}`)
+    })
     .put((req, res) => {
-        //update user
+        res.send(`Update the profile of user ${userId}`)
     })
     .delete((req, res) => {
-        //delete event
+        res.send(`Delete user ${userId}`)
     });
 
 module.exports = userRouter;
