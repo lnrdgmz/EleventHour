08efb81892c3a0e174555e89681d642808e2683d
Leo Gomez <leogomez@gmail.com>
2017-05-21 00:14:06 -0400

    feat: Add POST /events endpoint to handle event creation
    #10

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -2,25 +2,33 @@
 const eventRouter = require('express').Router();
 const eventUtils = require('../utils/eventUtils.js')
 
+const Event = require('../models/event');
 
 // Create route handles for events
 
 eventRouter.route('/')
-    .get((req, res) => {
-        //return events
-    })
-    .post((req, res) => {
-        //return events
-    });
-
+  .get((req, res) => {
+    //return events
+  })
+  .post((req, res) => {
+    const eventObj = req.body;
+    if (!['creator_id', 'title', 'description', 'date_time'].every(k => k in eventObj)) {
+      res.status(400).send();
+    } else {
+      new Event(eventObj).save()
+        .then(model => {
+          res.send(model);
+        })
+    }
+  });
 
 eventRouter.route('/:eventId')
-    .put((req, res) => {
-        //return events
-    })
-    .delete((req, res) => {
-        //return events
-    });
+  .put((req, res) => {
+    //return events
+  })
+  .delete((req, res) => {
+    //return events
+  });
 
 
 module.exports = eventRouter;
