acddfabceec0729338640652a51794bce64a8ad9
Alex Biczak <biczak@Alexs-MacBook-Pro.local>
2017-05-22 17:38:28 -0400

    feat(Browser): Profile and Menu Bar Skeletons
    Completed Profile and Menu Bar Layouts. Pending Routes.

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+// Import CSS
+import 'semantic-ui-react';
+import './styles/style.css';
+
+// Import React Components
+import React from 'react';
+import { render } from 'react-dom';
+import App from './Components/App.jsx';
+
+render(<App />, document.getElementById('app'));
