ebc78373f8aef9a634dc7cea7b0ca9ab6e72134d
Leo Gomez <leogomez@gmail.com>
2017-05-22 14:56:53 -0400

    Add cookie middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -8,18 +8,20 @@ const authRouter = require('../routes/authRoutes');
 const eventRouter = require('../routes/eventRoutes');
 const userRouter = require('../routes/userRoutes');
 const ratingRouter = require('../routes/ratingRoutes');
+const cookieParser = require('cookie-parser');
 
 // export middleware
 
 module.exports = (app, express) => {
-    app.use(morgan('dev'));
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(express.static(path.join(__dirname, '../../client')))
-    app.use(authRouter);
-    app.use('/events', eventRouter);
-    app.use('/users', userRouter);
-    app.use('/rating', ratingRouter);
-}
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, '../../client')));
+  app.use(authRouter);
+  app.use('/events', eventRouter);
+  app.use('/users', userRouter);
+  app.use('/rating', ratingRouter);
+};
