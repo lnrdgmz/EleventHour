ce8931812b9e2d57a80cbc5dc856880ec32671f7
Alex Biczak <biczak@Alexs-MacBook-Pro.local>
2017-05-22 22:07:35 -0400

    refactor(Redux): Began Redux Implementation
    Added Actions and Reducers folders, as well as files in each.  Began implementing Redux layout.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,18 @@
+// Import React Components
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 // Import CSS
 import 'semantic-ui-react';
 import './styles/style.css';
 
-// Import React Components
-import React from 'react';
-import { render } from 'react-dom';
+// Import Local Components
 import App from './Components/App.jsx';
+import reducer from './Reducers';
 
+// Create the Store
+const store = createStore(reducer);
+// Render our App
 render(<App />, document.getElementById('app'));
