79db23593ce46e5bad17d09c950da953ce9a7e84
Leo Gomez <leogomez@gmail.com>
2017-05-23 22:50:28 -0400

    Respond with user data or 'false' when making a GET request to /auth/loggedIn

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,80 +6,79 @@ const User = require('../models/user');
 
 /**
  * Check if a user is logged in by making a GET request to
- * /auth/loggedIn. Returns 'true' or 'false' in the body.
- *
- * Users can login via /auth/google or /auth/facebook
- *
- * After a successful login, the following cookies will be written:
- *  user_id: Number
- *  displayName: String
- *  events: an array of { event_id, user_role }
- *
- */
+  * /auth/loggedIn. If logged in, returns a JSON object with user's
+  * data and events. If not logged it, responds with 'false'.
+  *
+  * Users can login via /auth/google or /auth/facebook
+  *
+  * After a successful login, the following cookies will be written:
+  *  user_id: Number
+  *  displayName: String
+  *  events: an array of { event_id, user_role }
+  *
+  */
 
-const callbackHandler = (req, res) => {
-  /**
-   * Set some cookie data and send the appropriate response code
-   */
-  const userObj = new User({
-    oauth_provider: req.user.provider,
-    provider_id: req.user.id,
-  });
-  userObj.fetch({ withRelated: 'events' })
-    .then((model) => {
-      const maxAge = (moment.duration(7, 'days')).asMilliseconds();
-      if (model) {
-        req.session.user_id = model.get('id');
-        req.session.cookie.maxAge = maxAge;
-        /**
-         * TODO Add an events cookie. Depends on attendee table being populated.
-         * Events cookie is an object of { event_id, user_role }
-         */
-        const eventsCookieValue = [];
-        model.related('events').forEach((event) => {
-          const pivot = event.parse(event.pivot.attributes);
-          eventsCookieValue.push({ id: event.get('id'), role: pivot.flag });
-        });
-        res.cookie('userId', model.get('id'), { maxAge });
-        res.cookie('displayName', model.get('display_name'), { maxAge });
-        res.cookie('events', eventsCookieValue, { maxAge });
-        // res.send(model);
-        res.redirect(req.session.redirectTo || '/');
-      } else {
-        userObj.set({ display_name: req.user.displayName }).save()
-          .then((model) => {
-            req.session.user_id = model.get('id');
-            req.session.cookie.maxAge = maxAge;
-            res.cookie('userId', model.get('id'), { maxAge });
-            res.cookie('displayName', model.get('display_name'), { maxAge });
-            // res.send(`Added ${JSON.stringify(model)} to the database`);
-            res.redirect(req.session.redirectTo || '/');
-          });
-      }
+  const callbackHandler = (req, res) => {
+    /**
+     * Set some cookie data and send the appropriate response code
+     */
+    const userObj = new User({
+      oauth_provider: req.user.provider,
+      provider_id: req.user.id,
     });
-};
+    userObj.fetch({ withRelated: 'events' })
+      .then((model) => {
+        const maxAge = (moment.duration(7, 'days')).asMilliseconds();
+        if (model) {
+          req.session.user_id = model.get('id');
+          req.session.cookie.maxAge = maxAge;
+          const eventsCookieValue = [];
+          model.related('events').forEach((event) => {
+            const pivot = event.parse(event.pivot.attributes);
+            eventsCookieValue.push({ id: event.get('id'), role: pivot.flag });
+          });
+          res.cookie('userId', model.get('id'), { maxAge });
+          res.cookie('displayName', model.get('display_name'), { maxAge });
+          res.cookie('events', eventsCookieValue, { maxAge });
+          res.redirect(req.session.redirectTo || '/');
+        } else {
+          userObj.set({ display_name: req.user.displayName }).save()
+            .then((model) => {
+              req.session.user_id = model.get('id');
+              req.session.cookie.maxAge = maxAge;
+              res.cookie('userId', model.get('id'), { maxAge });
+              res.cookie('displayName', model.get('display_name'), { maxAge });
+              // res.send(`Added ${JSON.stringify(model)} to the database`);
+              res.redirect(req.session.redirectTo || '/');
+            });
+        }
+      });
+  };
 
-const recordReferer = (req, res, next) => {
-  req.session.redirectTo = req.get('Referer');
-  next();
-};
+  const recordReferer = (req, res, next) => {
+    req.session.redirectTo = req.get('Referer');
+    next();
+  };
 
-authRouter.get('/auth/google', recordReferer, passport.authenticate('google', { scope: ['profile'] }));
-authRouter.get('/auth/facebook', recordReferer, passport.authenticate('facebook'));
+  authRouter.get('/auth/google', recordReferer, passport.authenticate('google', { scope: ['profile'] }));
+  authRouter.get('/auth/facebook', recordReferer, passport.authenticate('facebook'));
 
 
-authRouter.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/fail' }),
-  callbackHandler);
+  authRouter.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/fail' }),
+    callbackHandler);
 
-authRouter.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/fail' }),
-  callbackHandler);
+  authRouter.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/fail' }),
+    callbackHandler);
 
-authRouter.get('/auth/loggedIn', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.send('true');
-  } else {
+  authRouter.get('/auth/loggedIn', (req, res) => {
+    if (req.isAuthenticated()) {
+      new User({ id: req.session.user_id }).fetch({ withRelated: 'events' })
+        .then(model => {
+          res.send(model);
+        });
+    } else {
     res.send('false');
   }
 });
