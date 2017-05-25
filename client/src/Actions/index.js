792ba18c09fa18b252321192197527a8a4cf8685
Kendrick Schuettler <keschuettler@gmail.com>
2017-05-25 16:14:11 -0400

    feat($CreateEvent): CreateEvent complete
    Finished validating CreateEvent form and successfully adding it to the database

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -24,15 +24,20 @@ export function createEvent(event) {
   event.date_time = moment(event.date + ':' + event.time);
   delete event.date;
   delete event.time;
-  console.log(event)
   return function (dispatch) {
     console.log('DISPATCHED');
     fetch('/events', {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(event),
     })
-    .then(resp => dispatch(addEvent(resp.data)))
+    .then((resp) => {
+      resp.json()
+        .then((body) => {
+          console.log(body);
+          dispatch(addEvent(resp.body));
+        });
+    })
     .catch(err => console.error(err));
   };
 }
--- client/src/Components/CreateEvents/createEvent.jsx
+++ client/src/Components/CreateEvents/createEvent.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import css from './createEvent.css';
 import { createEvent } from '../../Actions/index';
 import { Form, Input } from 'semantic-ui-react';
+import moment from 'moment';
 
 
 class CreateEvent extends Component {
@@ -16,6 +17,7 @@ class CreateEvent extends Component {
       skill_level: '',
       needs: '',
       description: '',
+      dateFlag: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -24,12 +26,20 @@ class CreateEvent extends Component {
 // Action <Creator></Creator>
   handleCreateClick = (e) => {
     e.preventDefault();
+    delete this.state.dateFlag;
     console.log(this.state);
     this.props.createEvent(this.state);
   }
 
   handleChange(event) {
     const name = event.target.name;
+    if (name === 'date' && moment(event.target.value).isBefore()) {
+      this.setState({ dateFlag: true });
+      return;
+    } else if (name === 'date' && !moment(event.target.value).isSameOrBefore()) {
+      this.setState({ dateFlag: false });
+    }
+    console.log(this.state);
     this.setState({ [name]: event.target.value });
   }
 
@@ -38,22 +48,23 @@ class CreateEvent extends Component {
     return (
       <div className="wrapper">
         <h1>Create a new Event</h1>
-        <form className="ui form">
+        <Form className="ui form" warning={this.state.dateFlag || this.state.timeFlag}>
           <Form.Field required>
             <label > Title</label>
             <Input name="title" type="text" placeholder="Name your event" required value={this.state.title} onChange={this.handleChange} />
           </Form.Field>
           <div className="equal width fields">
-            <Form.Field>
+            <Form.Field error={this.state.dateFlag} required>
               <label htmlFor> Date</label>
               <Input name="date" type="date" required onChange={this.handleChange} />
             </Form.Field>
-            <Form.Field>
+            <Form.Field required >
               <label>Time</label>
               <Input name="time" type="time" required onChange={this.handleChange} />
             </Form.Field>
           </div>
           <Form.Field name="needs" className="six wide" label="Spots to be filled" control="select" required onChange={this.handleChange}>
+            <option value=""> Choose a number of spots...</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -61,14 +72,32 @@ class CreateEvent extends Component {
             <option value="5">5</option>
           </Form.Field>
           <Form.Field name="skill_level" className="six wide" label="Required ability" control="select" required onChange={this.handleChange}>
+            <option value=""> Choose a skill rating...</option>
             <option value="1">You merely need a body</option>
             <option value="2">That body needs to be able to breath</option>
             <option value="3">That body is capable of playing the game</option>
             <option value="4">You look at that body and think, "It probably played competitive once."</option>
             <option value="5">You're pretty sure that body was at least a minor league professional athelete at one point.</option>
           </Form.Field>
-          <button type="submit" className="ui button" onClick={this.handleCreateClick} > Submit </button>
-        </form>
+          <Form.Field >
+              <label>Event Description</label>
+              <textarea name="description" type="textarea" placeholder="Elaborate on the event.." rows="5" onChange={this.handleChange} />
+            </Form.Field>
+          <div className="ui warning message">
+            <div className="content">
+              <div className="header">Warning</div>
+              <ul className="list">
+                <li className="content">Please review and make sure the time is after today</li>
+              </ul>
+            </div>
+          </div>
+          <button
+            type="submit"
+            disabled={this.state.dateFlag || this.state.timeFlag || !this.state.date || !this.state.title || !this.state.time || !this.state.skill_level || !this.state.needs}
+            className="ui button" 
+            onClick={this.handleCreateClick}
+          > Submit </button>
+        </Form>
       </div>
 
     );
