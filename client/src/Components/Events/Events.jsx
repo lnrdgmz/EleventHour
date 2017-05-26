956d7b61a6060b60436045d1ce877118d88306ed
Eric Zayas <ezayas16@icloud.com>
2017-05-26 15:16:05 -0400

    feat($component): filter by title
    Display will change based on filter
    
    BREAKING CHANGE: n/a
    
    filter by

--- client/src/Components/Events/Events.jsx
+++ client/src/Components/Events/Events.jsx
@@ -5,14 +5,17 @@ import Event from '.././Event/event.jsx';
 import Menu from '.././MenuBar/MenuBar.jsx';
 import {Link} from 'react-router-dom';
 import { Button, Header, Icon, Modal } from 'semantic-ui-react';
+import SearchBar from '.././SearchBar/searchBar.jsx';
 
  class Events extends React.Component {
    constructor(props){
      super(props);
      this.state={
        events: [],
-       toggle : false
+       toggle : false,
+       filterByTitle : ""
      };
+     this.onFilterEvents = this.onFilterEvents.bind(this);
    }
 
 
@@ -24,23 +27,34 @@ import { Button, Header, Icon, Modal } from 'semantic-ui-react';
        this.setState({events : parsedData});
      })
    }
-
+   onFilterEvents(event){
+     console.log(event.target.value);
+     this.setState({filterByTitle : event.target.value})
+   }
+  //  showFilteredData(){
+     
+    //}
    
-
+//  
    render() {
      return (
 
        <div>
         <Menu />
          <h1> Events </h1>
+         < SearchBar 
+         filterBy={this.state.filterByTitle}
+         onChange={this.onFilterEvents}
+        onFilterEvents={this.onFilterEvents}
+         /> <br />
          <article>
            {this.state.events.map((eventObject)=> (
             <div>
                <Event title={"Title:" + eventObject.title} description={eventObject.description} />
-                <Modal trigger={<Button>Basic Modal</Button>} basic size='small'>
+                <Modal trigger={<Button>Basic Modal</Button>} size='small'>
                 <Header content='Event Description' />
                 <Modal.Content>
-                <p> {eventObject.title}</p>
+                <p>{eventObject.title}</p>
                 <p>{eventObject.description}</p>
                 <p>{eventObject.needs}</p>
                 <p>{eventObject.location}</p>
