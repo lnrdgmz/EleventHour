f1f63e477d29baaaeabea670eb6cc9785aa7e640
Leo Gomez <leogomez@gmail.com>
2017-05-26 11:15:06 -0400

    refactor(user survey): Refactor SurveyComponent
    Add htmlFor attributes to labels and add propTypes to the component to
    appease the style guide

--- client/src/Components/Survey/SurveyComponent.jsx
+++ client/src/Components/Survey/SurveyComponent.jsx
@@ -1,48 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Survey = props => (
   <form onSubmit={props.handleSubmit}>
-    <label>
+    <label htmlFor="display-name-input">
       Display name
       <input
+        id="display-name-input"
         name="display_name"
         onChange={props.handleInputChange}
         type="text"
       />
     </label>
-    <label>
+    <label htmlFor="phone-number-input">
       Phone number
       <input
+        id="phone-number-input"
         name="contact_number"
         onChange={props.handleInputChange}
-        type="text"
+        type="tel"
       />
     </label>
-    <label>
+    <label htmlFor="email-input">
       Email addres
       <input
+        id="email-input"
         name="email"
         onChange={props.handleInputChange}
-        type="text"
+        type="email"
       />
     </label>
-    <label>
+    <label htmlFor="bio-input">
       Bio
       <input
+        id="bio-input"
         name="bio"
         onChange={props.handleInputChange}
         type="text"
       />
     </label>
-    <label>
+    <label htmlFor="age-input">
       Age
       <input
+        id="age-input"
         name="age"
         onChange={props.handleInputChange}
-        type="text"
+        type="number"
+        min="18"
+        max="120"
       />
     </label>
+    <button type="submit" ></button>
   </form>
 );
+
+Survey.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
 
 export default Survey;
