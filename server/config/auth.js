440e40e98686433e328a0cd59f39a99da3b3e0bd
Leo Gomez <leogomez@gmail.com>
2017-06-08 15:47:57 -0400

    Move session secret to .env file

--- server/config/auth.js
+++ server/config/auth.js
@@ -9,38 +9,36 @@ passport.use(new GoogleStrategy({
   callbackURL: 'http://localhost:3000/auth/google/callback',
 },
 (assessToken, refreshToken, profile, cb) => {
-  /** 
+  /**
    * TODO Find or create DB entry for user here
   */
   return cb(null, profile);
 }));
 
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
 },
 (assessToken, refreshToken, profile, cb) => {
-  /** 
+  /**
    * TODO Find or create DB entry for user here
   */
   return cb(null, profile);
 }));
 
-/** 
+/**
  * Edit this when implementing a user database?
  */
 passport.serializeUser((user, cb) => {
   cb(null, user);
 });
 passport.deserializeUser((obj, cb) => {
-  cb(null, obj)
+  cb(null, obj);
 });
 
 module.exports = (app) => {
-  app.use(session( { secret: 'gibson-43-chicken-dickens' } ));
+  app.use(session({ secret: process.env.SESSION_SECRET }));
   app.use(passport.initialize());
   app.use(passport.session());
 };
-
-
